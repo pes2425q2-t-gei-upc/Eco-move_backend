f7a327d7f4bf803bea6d8c9a40e30524d2e914e5
kunillet <herman.daniel.berrio@estudiantat.upc.edu>
2025-03-23 05:10:39 +0100

    retoques modelos

--- api_punts_carrega/models.py
+++ api_punts_carrega/models.py
@@ -1,25 +1,29 @@
-from enum import Enum
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
 
-class Velocitat_de_carrega(Enum):
+class Velocitat_de_carrega(models.TextChoices):
     LENTA = "Càrrega lenta"
     SEMI_RAPIDA = "Càrrega semi-ràpida"
     RAPIDA = "Càrrega ràpida"
     ULTRA_RAPIDA = "Càrrega ultra-ràpida"
     
 
-class Tipus_de_Corrent(Enum):
+class Tipus_de_Corrent(models.TextChoices):
     AC = "Corrent alterna"
     DC = "Corrent continua"
+
+class Resolucio(models.TextChoices):
+    USUARI_BLOQUEJAT = "Usuari bloquejat"
+    MISSATGE_ELIMINAT = "Missatge eliminat"
+    ABSOLT = "Absolt"
 
 class Punt(models.Model):
     id_punt = models.CharField(max_length=100, primary_key=True)
     lat = models.FloatField(null= True)
     lng = models.FloatField(null= True)
-    direccio = models.CharField(max_length=255,null=True)
-    ciutat = models.CharField(max_length=100,null= True)
-    provincia = models.CharField(max_length=100,null= True)
+    direccio = models.CharField(max_length=255,null=True,blank=True)
+    ciutat = models.CharField(max_length=100,null= True,blank=True)
+    provincia = models.CharField(max_length=100,null= True,blank=True)
 
     def __str__(self):
         return f"Punto {self.id_punt} en {self.lat}, {self.lng}"
@@ -34,34 +38,36 @@ class EstacioCarrega(Punt):
     
     def __str__(self):
         return f"Estació {self.id_punt} - {self.lat}, {self.lng}"
-    
+
+# Para que no se cree hasta que tengamos claros los campos
+# class RefugiClimatic(Punt):
+#     def __str__(self):
+#         return f"Refugi Climàtic: {self.direccio}"
+
 
 class PuntCarrega(models.Model):
     id_punt_carrega = models.CharField(max_length=100, primary_key=True)
     potencia = models.IntegerField()
-    tipus_velocitat = models.CharField(max_length=100,choices=Velocitat_de_carrega.__members__.items())
-    estacio = models.ForeignKey(EstacioCarrega, on_delete=models.SET_NULL, related_name='punt_carrega',null=True)
-    
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['estacio','id_punt_carrega'], name='unique_p_carrega')
-        ] 
+    tipus_velocitat = models.CharField(max_length=20, choices=Velocitat_de_carrega.choices)
+    estacio = models.ForeignKey(EstacioCarrega, on_delete=models.SET_NULL, related_name='punt_carrega',null=True) #blank=true?
+#    estacio = models.ForeignKey(EstacioCarrega, on_delete=models.CASCADE, related_name="punts_de_carrega")
     
     def __str__(self):
-        return f"Punt {self.id_punt_carrega} - {self.potencia}kW - {self.tipus_velocitat}"
+        return f"Punt de carrega {self.id_punt_carrega} - {self.potencia}kW - {self.tipus_velocitat}"
     
     
 class TipusCarregador(models.Model):
     id_carregador = models.CharField(max_length=100, primary_key=True)
     nom_tipus = models.CharField(max_length=100)
     tipus_connector = models.CharField(max_length=100)
-    tipus_corrent = models.CharField(max_length=100,choices=Tipus_de_Corrent.__members__.items())
+    tipus_corrent = models.CharField(max_length=100, choices=Tipus_de_Corrent.choices)
     punt_carrega = models.ManyToManyField(PuntCarrega,related_name='TipusCarregador')
+    #punt_carregadors = models.ManyToManyField(PuntCarrega, related_name='tipus_carregadors') sugerencia GPT, siguiendo la convencion de Django, aun no aplico por si rompe algo
 
     def __str__(self):
         return f"{self.nom_tipus} - {self.tipus_connector} ({self.tipus_corrent})"
 
-
+#Por revisar
 class Reserva(models.Model):
 
     estacion = models.ForeignKey(EstacioCarrega, on_delete=models.CASCADE, related_name='reservas')
