c672bcd13898ce25081407b2b5d52b8fbb07d404
heidihra <heidihra@stud.ntnu.no>
2025-04-16 01:09:40 +0200

    Write tests for alerts and chat

--- alerts_chat/tests/test_alerts.py
+++ alerts_chat/tests/test_alerts.py
@@ -1,154 +1,245 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import status
-import json
-from datetime import date, datetime, timedelta, time
-from api_punts_carrega.models import EstacioCarrega, Reserva
-from django.contrib.auth.models import User
+from datetime import datetime
+from api_punts_carrega.models import EstacioCarrega
+from alerts_chat.models import Missatge, PuntEmergencia, Chat
+
+User = get_user_model()
 
 class AlertsAPITests(TestCase):
     def setUp(self):
         # Create a test client
         self.client = Client()
         
-        # Create a test users
-        # Create sender and receiver users
         self.sender = User.objects.create_user(
+            email='sender@example.com',
             username='senderuser',
-            password='senderpassword'
+            password='senderpassword',
+            dni='12345678A',
         )
         self.receiver = User.objects.create_user(
+            email='receiver@example.com',
             username='receiveruser',
-            password='receiverpassword'
+            password='receiverpassword',
+            dni='87654321B',
         )
+        self.helper = User.objects.create_user(
+            email='helper@example.com',
+            username='helperuser',
+            password='helperpassword',
+            dni='11223344C',
+        )
+        self.sender_password = 'senderpassword'
+        self.receiver_password = 'receiverpassword'
+        self.helper_password = 'helperpassword'
         
         # Create a test station
         self.estacio = EstacioCarrega.objects.create(
-            nom='Test Station',
-            adreca='123 Test St',
-            latitud=41.123456,
-            longitud=2.123456,
-            potencia_maxima=50.0,
-            tipus_connector='Type 2'
+            id_punt="12345",
+            lat=41.3851,
+            lng=2.1734,
+            direccio="Carrer de Barcelona, 123",
+            ciutat="Barcelona",
+            provincia="Barcelona",
+            gestio="Public",
+            tipus_acces="targeta",
+            nplaces="2",
+        )
+        
+        self.data_alert = {
+            'titol': 'Test Alert',
+            'descripcio': 'This is a test alert.',
+            'lat': 41.123456,
+            'lng': 2.123456,
+            'is_active': True,
+        }
+        
+    def create_alert(self, user=None):
+        return self.client.post(
+            reverse('alerts-list'),
+            self.data_alert,
+            content_type='application/json'
+        )
+    
+    def create_chat(self, alert_id):
+        return self.client.post(
+            reverse('chat-create-chat', kwargs={'pk': alert_id}),
+            content_type='application/json'
+        )
+        
+    def send_message(self, chat_id, content):
+        return self.client.post(
+            reverse('messages-list'),
+            {
+                'chat': chat_id,
+                'content': content,
+            },
+            content_type='application/json'
         )
     
     
     # Test creating a new alert
     def test_creating_alert(self):
-        url = reverse('alerts-list')
-        data = {
-            'titol': 'Test Alert',
-            'descripcio': 'This is a test alert.',
-            'latitud': 41.123456,
-            'longitud': 2.123456,
-            'is_active': True,
-            'sender': self.sender.pk
-        }
-        
-        self.client.login(username='senderuser', password='senderpassword')
-        
-        response = self.client.post(url, data, format='json')
+        response = self.client.login(email=self.sender.email, password=self.sender_password)
+        self.assertTrue(response, "Login failed")
+        alert = self.create_alert()
         
         # Check if the alert was created successfully
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(alert.status_code, status.HTTP_201_CREATED)
         
         # Check if the alert data is correct
-        alert_data = response.json()
-        self.assertEqual(alert_data['titol'], data['titol'])
-        self.assertEqual(alert_data['descripcio'], data['descripcio'])
-        self.assertEqual(alert_data['latitud'], data['latitud'])
-        self.assertEqual(alert_data['longitud'], data['longitud'])
+        alert_data = alert.json()
+        self.assertEqual(alert_data['titol'], self.data_alert['titol'])
+        self.assertEqual(alert_data['descripcio'], self.data_alert['descripcio'])
+        self.assertEqual(alert_data['lat'], self.data_alert['lat'])
+        self.assertEqual(alert_data['lng'], self.data_alert['lng'])
         self.assertTrue(alert_data['is_active'])
     
     
     # Test creating chat from an alert
     def test_creating_chat(self):
-        url = reverse('alerts-create_chat', kwargs={'pk': self.estacio.pk})
-        
         # Create a new alert
-        self.client.login(username='senderuser', password='senderpassword')
-        alert_response = self.client.post(reverse('alerts-list'), {
-            'titol': 'Test Alert',
-            'descripcio': 'This is a test alert.',
-            'latitud': 41.123456,
-            'longitud': 2.123456,
-            'is_active': True,
-            'sender': self.sender.pk
-        }, format='json')
+        self.client.login(email=self.sender.email, password=self.sender_password)
+        alert = self.create_alert().json()
         
-        alert_data = alert_response.json()
-        
-        response = self.client.post(url, {'alert_id': alert_data['id']}, format='json')
+        chat = self.create_chat(alert['id_emergencia'])
         
         # Check if the chat was created successfully
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(chat.status_code, status.HTTP_201_CREATED)
         
         # Check if the chat data is correct
-        chat_data = response.json()
-        self.assertEqual(chat_data['alerta'], alert_data['id'])
+        chat_data = chat.json()
+        self.assertEqual(chat_data['alerta'], alert['id_emergencia'])
         self.assertEqual(chat_data['creador'], self.sender.pk)
+    
     
     # Test sending messages in a chat
     def test_sending_message_in_chat(self):
-
-        self.client.login(username='senderuser', password='senderpassword')
-        alert_response = self.client.post(reverse('alerts-list'), {
-            'titol': 'Test Alert',
-            'descripcio': 'This is a test alert.',
-            'latitud': 41.123456,
-            'longitud': 2.123456,
-            'is_active': True,
-            'sender': self.sender.pk
-        }, format='json')
+        # The sender logs in, creates an alert, and logs out
+        self.client.login(email=self.sender.email, password=self.sender_password)
+        alert = self.create_alert().json()
+        self.client.logout()
         
-        alert_data = alert_response.json()
+        # The receiver logs in and creates a chat (is the helper for the sender)
+        self.client.login(email=self.receiver.email, password=self.receiver_password)
+        chat = self.create_chat(alert['id_emergencia']).json()
         
-        chat_response = self.client.post(reverse('alerts-create_chat', kwargs={'pk': alert_data['id']}), {
-            'alert_id': alert_data['id']
-        }, format='json')
-        
-        chat_data = chat_response.json()
-        
-        url = reverse('messages-list')
-        data = {
-            'missatge': 'Hello, this is a test message.',
-            'chat': chat_data['id'],
-            'sender': self.sender.pk
-        }
-        
-        response = self.client.post(url, data, format='json')
-        
-        # Check if the message was sent successfully
+        # The receiver sends the first message
+        response = self.send_message(chat['id'], "Hello, I see your alert.")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()['content'], "Hello, I see your alert.")
+        self.client.logout()
         
-        # Check if the message data is correct
-        message_data = response.json()
-        self.assertEqual(message_data['missatge'], data['missatge'])
-        
-        data_2 = {
-            'missatge': 'Hello, I am responding to you message.',
-            'chat': chat_data['id'],
-            'receiver': self.receiver.pk
-        }
-        response_2 = self.client.post(url, data, format='json')
-        
-        # Check if the message was sent successfully
+        # The sender replies
+        self.client.login(email=self.sender.email, password=self.sender_password)
+        response_2 = self.send_message(chat['id'], "Thank you for your response.")
         self.assertEqual(response_2.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response_2.json()['content'], "Thank you for your response.")
         
-        # Check if the message data is correct
-        message_data_2 = response_2.json()
-        self.assertEqual(message_data_2['missatge'], data['missatge'])
-    
-    # Test getting messages from a chat
+        # Now retrieve messages
+        response_3 = self.client.get(reverse('chat-get-messages', kwargs={'pk': chat['id']}))
+        self.assertEqual(response_3.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response_3.json()), 2)
+        
+        self.assertEqual(response_3.json()[0]['content'], "Hello, I see your alert.")
+        self.assertEqual(response_3.json()[1]['content'], "Thank you for your response.")
+
     
     # Test getting all chats for a user
+    def test_getting_multiple_chats_for_user(self):
+        # Sender 1 logs in and creates an alert
+        self.client.login(email=self.sender.email, password=self.sender_password)
+        alert1 = self.create_alert().json()
+        self.client.logout()
+
+        # Sender 2 logs in and creates another alert
+        self.client.login(email=self.receiver.email, password=self.receiver_password)
+        alert2 = self.create_alert().json()
+        self.client.logout()
+
+        # Helper logs in and creates a chat for both alerts
+        self.client.login(email=self.helper.email, password=self.helper_password)
+        chat1 = self.create_chat(alert1['id_emergencia'])
+        self.assertEqual(chat1.status_code, status.HTTP_201_CREATED)
+        
+        chat2 = self.create_chat(alert2['id_emergencia'])
+        self.assertEqual(chat2.status_code, status.HTTP_201_CREATED)
+
+        # Get all chats for the helper
+        response = self.client.get(reverse('chat-my-chats'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        chats = response.json()
+        self.assertEqual(len(chats), 2)
+
+        # Ensure both alerts are in the chats returned
+        alert_ids = {chat['alerta'] for chat in chats}
+        self.assertIn(alert1['id_emergencia'], alert_ids)
+        self.assertIn(alert2['id_emergencia'], alert_ids)
+
+
+    # Test unauthorized access to chat messages
+    def test_unauthorized_alert_creation(self):
+        response = self.create_alert()
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
     
+    # Test get empty chat messages
+    def test_get_empty_chat_messages(self):
+        self.client.login(username=self.sender.email, password=self.sender_password)
         
+        alert = self.create_alert().json()
         
+        chat = self.create_chat(alert['id_emergencia'])
+        self.assertEqual(chat.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.get(reverse('chat-get-messages', kwargs={'pk': chat.json()['id']}))
         
-    def test_modificar_reserva(self):
-        print()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), [])
+
+
+    # Test sending empty message
+    def test_sending_empty_message(self):
+        self.client.login(username=self.sender.email, password=self.sender_password)
+        alert = self.create_alert().json()
+        chat = self.create_chat(alert['id_emergencia']).json()
+
+        response = self.send_message(chat['id'], '')
         
-    def test_eliminar_reserva(self):
-        url = self.reserva_url + f"{self.reserva.pk}/eliminar/"
+        # Check if the message was not sent
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    
+    # Test chat is deactivated
+    def test_chat_deactivation(self):
+        self.client.login(email=self.sender.email, password=self.sender_password)
+        alert = self.create_alert().json()
+        chat = self.create_chat(alert['id_emergencia']).json()
+        
+        response = self.client.patch(reverse('chat-detail', kwargs={'pk': chat['id']}), {
+            'activa': False
+        }, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.json()['activa'])
+
+    
+    # Test marking messages as read
+    def test_marking_messages_as_read(self):
+        self.client.login(email=self.sender.email, password=self.sender_password)
+        alert = self.create_alert().json()
+        chat = self.create_chat(alert['id_emergencia']).json()
+
+        message = self.send_message(chat['id'], "Test message.").json()
+
+        # Mark the message as read
+        response = self.client.patch(reverse('messages-detail', kwargs={'pk': message['id_missatge']}), {
+            'is_read': True
+        }, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.json()['is_read'])
+    
