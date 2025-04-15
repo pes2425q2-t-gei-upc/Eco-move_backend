47dae82ed5864bed419a6a2571e44b5f5b0f1b53
heidihra <heidihra@stud.ntnu.no>
2025-04-16 01:08:36 +0200

    Add read_only fields and ordering for serializers

--- alerts_chat/serializers.py
+++ alerts_chat/serializers.py
@@ -10,9 +10,11 @@ class AlertSerializer(serializers.ModelSerializer):
     class Meta:
         model = PuntEmergencia
         fields = '__all__'
+        read_only_fields = ['id_emergencia', 'timestamp', 'sender']
         
 class MessagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Missatge
         fields = '__all__'
-        read_only_fields = ['id_emergencia', 'timestamp', 'sender']
+        ordering = ['timestamp']
+        read_only_fields = ['timestamp', 'sender']
